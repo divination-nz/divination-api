09fe032645d46c409cbc679d6e9e0f6cdb9a6b17
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-03-03 16:19:05 +1300

    Configure Swagger Doc options

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 using Divination.Data.Repository;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var rulesRepository = new  RulesRepository();
+var rulesRepository = new RulesRepository();
 builder.Services.AddSingleton<IRulesRepository>(rulesRepository);
 
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Divination API",
+        Version = "v1.0.0",
+        Description = "This is a Web API for fetching rules text from Magic: The Gathering's comprehensive rules."
+    });
+});
 
 var app = builder.Build();
 
