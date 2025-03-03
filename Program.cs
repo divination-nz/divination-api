c870e7930b83dbb6f0a507e637d33f607163bcb1
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-03-03 15:41:21 +1300

    Initialise rules repository on app start

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddSingleton<IRulesRepository, RulesRepository>();
+var rulesRepository = new  RulesRepository();
+builder.Services.AddSingleton<IRulesRepository>(rulesRepository);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
