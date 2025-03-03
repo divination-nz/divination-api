9d8ab7a068b6be1cad1f6fbcbcb204b9874c42a3
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2025-03-03 16:11:26 +1300

    Expose swagger in release builds

--- Program.cs
+++ Program.cs
@@ -2,23 +2,20 @@ using Divination.Data.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 var rulesRepository = new  RulesRepository();
 builder.Services.AddSingleton<IRulesRepository>(rulesRepository);
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
